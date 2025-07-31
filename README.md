1c85a000a304e2551df0b8e079eb322d23f5b1f1
Nighty3098 <night3098games@gmail.com>
2025-07-31 17:15:14 +0700

    First release

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # ProxySniffer
-ProxySniffer
+
+![alt text](imgs/1.png)
+
+This Python application downloads proxy lists from multiple sources by proxy type, checks their validity and outputs the list of working proxies. It supports HTTP, HTTPS, SOCKS4, and SOCKS5 proxy types.
+
+## Features
+
+- Loads proxy lists from predefined URLs for HTTP, HTTPS, SOCKS4, and SOCKS5.
+- Checks proxy availability by making a test request through each proxy.
+- Multithreaded proxy validation for faster processing.
+- User selects proxy type and number of proxies to check.
+- Outputs the list of working proxies.
+
+## Install
+
+```bash
+
+git clone https://github.com/He4vyL0v3/ProxySniffer
+cd ProxySniffer
+pip3 install -r req.txt
+cd src 
+python3 main.py
+
+```
+
+![alt text](imgs/2.png)
---
+++ src/links.py
@@ -0,0 +1,42 @@
+proxy_HTTP = [
+    "https://api.openproxylist.xyz/http.txt",
+    "https://alexa.lr2b.com/proxylist.txt",
+    "https://rootjazz.com/proxies/proxies.txt",
+    "https://www.proxy-list.download/api/v1/get?type=http",
+    "https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/http/http.txt",
+    "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt",
+    "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all",
+    "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt",
+]
+
+proxy_HTTPS = [
+    "https://www.sslproxies.org/",
+    "https://www.proxy-list.download/api/v1/get?type=https",
+    "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=https&timeout=10000&country=all&ssl=all&anonymity=all",
+    "https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/https/https.txt",
+    "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-https.txt",
+]
+
+proxy_SOCKS4 = [
+    "https://api.openproxylist.xyz/socks4.txt",
+    "https://www.proxy-list.download/api/v1/get?type=socks4",
+    "https://www.socks-proxy.net/",
+    "https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/socks4/socks4.txt",
+    "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/socks4.txt",
+    "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt",
+    "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=SOCKS4&timeout=10000&country=all&ssl=all&anonymity=all",
+    "https://raw.githubusercontent.com/rdavydov/proxy-list/main/proxies_anonymous/socks4.txt",
+]
+
+proxy_SOCKS5 = [
+    "https://www.proxy-list.download/api/v1/get?type=socks5",
+    "https://api.openproxylist.xyz/socks5.txt",
+    "https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/socks5/socks5.txt",
+    "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/socks5.txt",
+    "https://raw.githubusercontent.com/elliottophellia/yakumo/master/results/socks5/global/socks5_checked.txt",
+    "https://raw.githubusercontent.com/ErcinDedeoglu/proxies/main/proxies/socks5.txt",
+    "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt",
+    "https://raw.githubusercontent.com/rdavydov/proxy-list/main/proxies_anonymous/socks5.txt",
+    "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=SOCKS5&timeout=10000&country=all&ssl=all&anonymity=all",
+    "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-socks5.txt",
+]
